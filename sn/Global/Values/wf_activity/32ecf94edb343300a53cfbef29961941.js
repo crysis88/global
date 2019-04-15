b3dc43a5623cc141b6f3eed0e6191243da3d093d
System Administrator <sverma122013@gmail.com>
2019-04-15 11:50:21 +0000

    Changes by 'System Administrator' on 'Demo 2' -- Build #1

---
+++ sn/Global/Values/wf_activity/32ecf94edb343300a53cfbef29961941.js
@@ -0,0 +1,21 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_variable_value
+ * Created On  : 2019-04-15 11:48:01
+ * Created By  : admin
+ * Updated On  : 2019-04-15 11:48:01
+ * Updated By  : admin
+ * URL         : /sys_variable_value.do?sys_id=32ecf94edb343300a53cfbef29961941
+ */
+// This script needs to set answer to 'yes' or 'no' to indicate the state of the activity.
+//
+// For example,
+//
+//   answer = ifScript();
+//
+//   function ifScript() {
+//      if (condition is true) {
+//         return 'yes';
+//      }
+//      return 'no';
+//   }
