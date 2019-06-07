40dbbd64ed94b710d78559a7d14e781ff33f15ef
System Administrator <admin@example.com>
2019-06-07 10:32:40 +0000

    Changes by 'System Administrator' on '7_june 2' -- Build #1

---
+++ sn/Global/Script Include/active_true/global.Test.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-06-07 10:31:32
+ * Created By  : admin
+ * Updated On  : 2019-06-07 10:31:32
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=ada7e2cf4f713300a3cc121f9310c711
+ */
+var Test = Class.create();
+Test.prototype = {
+    initialize: function() {
+    },
+
+    type: 'Test'
+};
